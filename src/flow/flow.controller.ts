55fb80c89a956b7eb264190da860ce56278b6fe8
Fopa Kuete Duclair <fopaduclair2000@gmail.com>
2026-06-14 10:36:02 +0100

    fix: return HTTP 200 from flow data exchange endpoint (NestJS POST defaults to 201)

--- src/flow/flow.controller.ts
+++ src/flow/flow.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, Res } from '@nestjs/common'
+import { Controller, HttpCode, Post, Req, Res } from '@nestjs/common'
 import type { Request, Response } from 'express'
 import { FlowDataExchangeService } from './flow-data-exchange.service'
 
@@ -7,6 +7,7 @@ export class FlowController {
   constructor(private readonly flowDataExchange: FlowDataExchangeService) {}
 
   @Post('flow-data-exchange')
+  @HttpCode(200)
   async handleDataExchange(@Req() req: Request, @Res() res: Response) {
     try {
       await this.flowDataExchange.handleDataExchange(req, res)
